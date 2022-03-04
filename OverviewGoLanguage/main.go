46484385cd477671e2571f94ef387c39e0481e53
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-05 01:55:03 +0300

    [atakan] create integer var

--- OverviewGoLanguage/main.go
+++ OverviewGoLanguage/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	var myFirstMessage string
+	var myNumber int
 	myFirstMessage = "Hello, Go Lang. This is my first message"
+	myNumber = 22
 	fmt.Println(myFirstMessage)
+	fmt.Printf("My age is %d\n", myNumber)
 }
