ad2fad5ed275f97435c5a3a0f1fd84693ab97d90
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-05 20:47:51 +0300

    [atakan] Packages

--- OverviewGoLanguage/main.go
+++ OverviewGoLanguage/main.go
@@ -1,53 +1,22 @@
 package main
 
-import "fmt"
-
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+import (
+	"fmt"
+	"github/atakanteko/golangplayground/helpers"
+)
 
 func main() {
-	dog := Dog{
-		Name:  "Soldier",
-		Breed: "German Shephers",
+	var myVar []helpers.SomeType
+
+	myVar = append(myVar, helpers.SomeType{
+		TypeName:   "Gamers",
+		TypeNumber: 1,
+	}, helpers.SomeType{
+		TypeName:   "Developers",
+		TypeNumber: 2,
+	})
+	fmt.Println(myVar)
+	for _, info := range myVar {
+		fmt.Println("Type Name:", info.TypeName, "Type Number:", info.TypeNumber)
 	}
-	gorilla := Gorilla{
-		Name:          "Big Boi",
-		Color:         "Gray",
-		NumberOfTeeth: 28,
-	}
-	PrintInfo(&dog)
-	PrintInfo(&gorilla)
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animail says", a.Says(), "and has", a.NumberOfLegs())
-}
-
-func (d *Dog) Says() string {
-	return "Woof"
-}
-
-func (d *Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (d *Gorilla) Says() string {
-	return "Woooh"
-}
-
-func (d *Gorilla) NumberOfLegs() int {
-	return 2
 }
